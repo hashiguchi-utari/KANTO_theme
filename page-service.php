e1cbb12b2c00ad52d2731b28547eb41dcd160423
naokimorita <moritanaoki.utari@gmail.com>
2026-08-24 14:54:48 +0900

    service作成始めました。

---
+++ page-service.php
@@ -0,0 +1,139 @@
+<?php get_header(); ?>
+
+<main>
+
+  <section class="service_main">
+    <div class="service_left">
+      <div class="service_message">
+        <h1 class="catchcopy">あなたの空に<br><span>そっと寄り添う。</span></h1>
+        <p class="service_guide">
+          ひとりひとりの想いに耳を傾け、<br>
+          安心して自分らしく過ごせる明日へ、<br>
+          看護師がご自宅に訪問し、<br>
+          心と体のバランスを整えるお手伝いをします。</p>
+      </div>
+
+      <div class="icon_set">
+        <div class="heart">
+          <img class="icon" src="<?php echo get_template_directory_uri(); ?>/img/ハート 1.png" alt="ハート">
+          <p class="pr_point">こころと体に<br>やさしく寄り添う</p>
+        </div>
+        <div class="speak">
+          <img class="icon" src="<?php echo get_template_directory_uri(); ?>/img/対話 2.png" alt="対話">
+          <p class="pr_point">対話を大切にした<br>サポート</p>
+        </div>
+        <div class="grouphome">
+          <img class="icon" src="<?php echo get_template_directory_uri(); ?>/img/ホーム 1.png" alt="ホーム">
+          <p class="pr_point">グループホーム<br>SETAと連携</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="service_img">
+      <img class="speak_human" src="<?php echo get_template_directory_uri(); ?>/img/看護師と利用者が対話してる様子 1.png" alt="対話の写真">
+      <div class="img_photo">
+        <img class="photo" src="<?php echo get_template_directory_uri(); ?>/img/MVイメージ画像1 1.png" alt="散歩の写真">
+        <img class="photo" src="<?php echo get_template_directory_uri(); ?>/img/イメージ画像.png" alt="散歩の写真">
+        <img class="photo" src="<?php echo get_template_directory_uri(); ?>/img/イメージ画像.png" alt="散歩の写真">
+      </div>
+    </div>
+  </section>
+
+  <section class="service_content">
+    <div class="service_info">
+      <div class="top_word">
+        <h2>サービス内容</h2>
+        <span class="line"></span>
+        <p>一人ひとりの状態や気持ちに寄り添い、安心して暮らせる毎日をサポートします。</p>
+      </div>
+      <div class="body_maintenance">
+        <img class="photo" src="<?php echo get_template_directory_uri(); ?>/img/イメージ画像.png" alt="血圧計">
+        <h3></h3>
+        <p></p>
+
+      </div>
+
+      <div class="medicine">
+        <img class="photo" src="<?php echo get_template_directory_uri(); ?>/img/イメージ画像.png" alt="薬の写真">
+        <h3></h3>
+        <p></p>
+      </div>
+
+      <div class="main_service">
+        <img class="photo" src="<?php echo get_template_directory_uri(); ?>/img/イメージ画像.png" alt="対話してる写真">
+        <h3></h3>
+        <p></p>
+      </div>
+
+    </div>
+
+
+
+
+  </section>
+
+
+  <div class="triangle">
+
+    <!-- 線 -->
+    <span class="line line_left1"></span>
+    <span class="line line_left2"></span>
+
+    <span class="line line_right1"></span>
+    <span class="line line_right2"></span>
+
+    <span class="line line_bottom1"></span>
+    <span class="line line_bottom2"></span>
+
+    <img class="kanto_img" src="<?php echo get_template_directory_uri(); ?>/img/Group 88.png" alt="カントイメージ">
+    <img class="seta_img" src="<?php echo get_template_directory_uri(); ?>/img/Mask group.png" alt="セタイメージ">
+    <img class="utari_img" src="<?php echo get_template_directory_uri(); ?>/img/Mask group3.png" alt="ウタリイメージ">
+    <p class="perple">グループ紹介</p>
+    <div class="group_name">
+      <p>SETA</p>
+      <p>UTARI</p>
+    </div>
+  </div>
+  </div>
+
+  <section class="seta">
+    <h2>障がい者グループホーム【セタ】</h2>
+    <div class="seta_photo">
+      <img class="seta1" src="<?php echo get_template_directory_uri(); ?>/img/image 37.png" alt="セタ１">
+      <img class="seta2" src="<?php echo get_template_directory_uri(); ?>/img/image 38.png" alt="セタ２">
+      <img class="seta3" src="<?php echo get_template_directory_uri(); ?>/img/image 39.png" alt="セタ３">
+      <img class="seta4" src="<?php echo get_template_directory_uri(); ?>/img/image 40.png" alt="セタ４">
+    </div>
+    <h3>『安心と繋がりがある暮らし』</h3>
+    <div class="guide">
+      <p>男性専用のグループホーム・女性専用のグループホーム、男性女性可能のアパートタイプ、男性シェアハウスまで幅広く・・・。
+        あなたらしさを大切にした快適な生活を送りませんか？</p>
+      <a class="seta_button" href="https://seta-gh.jp/">
+        グループホーム<span>【セタ】　HP</span>
+      </a>
+    </div>
+  </section>
+
+  <section class="utari">
+    <h2>就労継続支援A型Ｂ型事業所ＵＴＡＲＩ-ウタリ-</h2>
+    <div class="utari_photo">
+      <img class="utari1" src="<?php echo get_template_directory_uri(); ?>/img/image 43.png" alt="ウタリ１">
+      <img class="utari2" src="<?php echo get_template_directory_uri(); ?>/img/image 41.png" alt="ウタリ２">
+      <img class="utari3" src="<?php echo get_template_directory_uri(); ?>/img/image 42.png" alt="ウタリ３">
+    </div>
+    <h3>『ひとりじゃない』</h3>
+    <div class="guide">
+      <p>仲間と共に学び、働き、時には支え合いながら自分のペースで成長できる場を提供します。WEB制作や動画編集といった仕事を通じて、自分のペースで「できること」を少しずつ広げ、社会とのつながりを実感できる場を目指しています。 </p>
+      <a class="utari_button" href="https://utari.jp/">
+        UTARI<span>HP</span>
+      </a>
+    </div>
+  </section>
+
+</main>
+
+<?php get_footer(); ?>
+
+</body>
+
+</html>
