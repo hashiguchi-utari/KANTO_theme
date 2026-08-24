309f723a1a29a9f10b32029ee6621b52a2fc4f3b
maki suzuki <suzukimaki.utari@gmail.com>
2026-08-24 11:24:55 +0900

    初期保存

---
+++ front-page.php
@@ -0,0 +1,47 @@
+<?php get_header(); ?>
+
+<!-- ここに本文を記載 -->
+<main class="front_container">
+  <section class="front_mv">
+    <img src="<?php echo get_theme_file_uri('/img/front_page/movie-image.png'); ?>" alt="TOP動画部分画像で仮置き">
+  </section>
+
+
+  <section>
+    <div class="front-kanto">
+      <img src="<?php echo get_theme_file_uri('/img/front_page/vector1.png'); ?>" alt="背景ベクター">
+      <div class="front-howto">
+        <h3 class="h3">KANTOとは</h3>
+        <p>訪問看護ステーションKANTOは、ご利用者様やご家族が住み慣れた地域で安心して生活を続けられるよう、一人ひとりに寄り添った看護を提供する訪問看護ステーションです。
+        </p>
+      </div>
+      <img src="<?php echo get_theme_file_uri('/img/front_page/1.png'); ?> alt="画像１">
+    </div>
+  </section>
+
+  <section>
+    「自分らしい」を
+    一緒に育てる訪問看護
+
+    利用までの流れ
+    サービス内容
+    グループ紹介
+  </section>
+
+  <section>
+    お知らせ
+  </section>
+
+  <section>
+    Instagram
+    訪問看護ステーションKANTO
+
+    障がい者グループホームぐ🍀セタ
+
+    就労継続支援A型B型事業所UTARI-ウタリ-
+  </section>
+
+</main>
+<!-- ここまで本文を記載 -->
+
+<?php get_footer(); ?>
